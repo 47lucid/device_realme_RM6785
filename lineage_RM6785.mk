9616439b59e59523507a5795c2a7385463987361
47lucid <136931389+47lucid@users.noreply.github.com>
2024-03-17 08:06:46 +0530

    Update lineage_RM6785.mk

--- lineage_RM6785.mk
+++ lineage_RM6785.mk
@@ -15,12 +15,6 @@ $(call inherit-product, device/realme/RM6785/device.mk)
 # Inherit some common Paranoid OS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit basic call recoder, Samsung Music and Gcam.
-$(call inherit-product, vendor/bcr/bcr.mk)
-$(call inherit-product-if-exists, vendor/gms/gms_pico.mk)
-$(call inherit-product-if-exists, vendor/SamsungMusic/config.mk)
-
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_RM6785
 PRODUCT_DEVICE := RM6785
